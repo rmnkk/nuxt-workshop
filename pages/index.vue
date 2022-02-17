af591944f61cf7873aa668a3c029abc9db1b9ed7
Roman <romikkus@gmail.com>
2022-02-17 20:11:20 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <p>The cute dog below</p>
+    <img :src="imgSrc" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({ $axios }) {
+    const response = await $axios.get('https://dog.ceo/api/breeds/image/random')
+    const imgSrc = response.data.message
+
+    return { imgSrc }
+  },
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
